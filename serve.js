4d638c7b8376da716998a678230de74403fd886c
jugalsuthar4 <jugalsutharpro@gmail.com>
2025-03-22 19:28:23 +0530

    rtps tohls

---
+++ serve.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const cors=require("cors")
+
+const app = express();
+app.use(cors())
+
+
+app.use("/hls", express.static(path.join(__dirname, "output")));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`HLS Server running at http://localhost:${PORT}/hls/index.m3u8`);
+});
